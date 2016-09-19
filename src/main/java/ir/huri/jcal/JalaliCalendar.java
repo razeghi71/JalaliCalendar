3865725aa50f305930088d49e8600b1c069b7cea
Mohammad Razeghi <razeghi71@gmail.com>
2016-09-19 14:06:31 +0430

    add some string get functions

--- src/main/java/ir/huri/jcal/JalaliCalendar.java
+++ src/main/java/ir/huri/jcal/JalaliCalendar.java
@@ -117,6 +117,52 @@ public class JalaliCalendar {
 
     /**
      *
+     * @return month name
+     */
+    public String getMonthString() {
+        switch (getMonth()) {
+            case 1:
+                return "فروردین";
+            case 2 :
+                return "اردیبهشت";
+            case 3:
+                return "خرداد";
+            case 4:
+                return "تیر";
+            case 5:
+                return "مرداد";
+            case 6:
+                return "شهریور";
+            case 7:
+                return "مهر";
+            case 8:
+                return "آبان";
+            case 9:
+                return "آذر";
+            case 10:
+                return "دی";
+            case 11:
+                return "بهمن";
+            case 12:
+                return "اسفند";
+            default:
+                return "نامعلوم";
+        }
+    }
+
+
+    /**
+     * get String with the following format :
+     *  یکشنبه ۱۲ آبان
+     * @return String format
+     */
+
+    public String getDayOfWeekDayMonthString() {
+        return getDayOfWeekString() + " " + getDay() + " " + getMonthString() ;
+    }
+
+    /**
+     *
      * @return return whether this year is a jalali leap year
      */
     public boolean isLeap() {
